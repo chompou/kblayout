4ffed8a7e8511645fd19b60005c8ecacf0839510
Tobias Giverholt <tobias.giverholt@autopay.io>
2024-02-01 07:44:59 +0100

    first commit

---
+++ rules.mk
@@ -0,0 +1,6 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+# RGB_MATRIX_ENABLE = no
+OLED_ENABLE     = yes
+OLED_TIMEOUT = 0
+LTO_ENABLE      = yes
